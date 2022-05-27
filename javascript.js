056f4516d2f23896db31e3f2e9a9ab0dc9b76b75
Recep8 <recepkarabulut23@gmail.com>
2022-05-28 02:50:57 +0300

    yeni

---
+++ javascript.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",init)
+function myfunction(){
+    document.getElementById("buton").innerHTML = "Recep";
+
+
+}
+function init(){
+    document.getElementById("buton").addEventListener("click",myfunction);
+    document.getElementById("").addEventListener("")
+
+}
