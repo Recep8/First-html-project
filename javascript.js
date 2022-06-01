7c83c3dadefb0ad47f99824075933ab72301d44a
Recep8 <recepkarabulut23@gmail.com>
2022-06-02 00:12:19 +0300

    new

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function logolar_out(){
 
 }
 function yazi_yazdir(){
-    document.getElementById("paragraf").innerHTML = "İsmim Recep Karabulut";
+    document.getElementById("yazi").innerHTML = "İsmim Recep Karabulut\nBandırma Onyedi Eylül Üniversitesinde öğrenim görmekteyim\nBilgisayar Mühendisliği 2. Sınıf öğrencisiyim\nBildiğim diller sağ taraftaki tabloda bulunmaktadır\nBana ulaşmak isterseniz sayfanın en altında bağlantılarım bulunmaktadır\nYada direkt olarak üstte bulunan contact kısmına tıklayarak\nalttaki bağlantı kısmına ulaşabilirsiniz.";
 }
         
 function init(){
